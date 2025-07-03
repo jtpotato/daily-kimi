26d92e18b235aeb535a35927c06879757cf9681b
Joel <58995538+jtpotato@users.noreply.github.com>
2025-07-03 13:26:15 +1000

    Add upload functionality and update images.json with new image URLs and captions

---
+++ upload.ts
@@ -0,0 +1,44 @@
+import { UTApi } from "uploadthing/server";
+import sharp from "sharp";
+import fs from "fs";
+import { exit } from "process";
+
+// read caption from caption.txt
+const caption = await Bun.file("caption.txt").text();
+
+// read buffer from file named 'imagedump' with no extension
+const imageBuffer = await Bun.file("imagedump").arrayBuffer();
+// convert image to jpeg
+const jpegBuffer = await sharp(imageBuffer).jpeg().toBuffer();
+
+// save to file for testing
+fs.writeFileSync("imagedump.jpg", jpegBuffer);
+
+// make blob from jpeg buffer
+const blob = new Blob([jpegBuffer], { type: "image/jpeg" });
+const jsFile = new File([blob], "imagedump.jpg", { type: "image/jpeg" });
+
+// upload to uploadthing
+const utapi = new UTApi({ token: process.env.UPLOADTHING_TOKEN || "" });
+const res = await utapi.uploadFiles([jsFile]);
+
+let urls = res.map((file) => file.data?.ufsUrl);
+
+// edit images.json to include image url and caption.
+const imagesJsonPath = "images.json";
+let imagesJson: { images: { link: string; caption: string }[] } = {
+  images: [],
+};
+
+if (fs.existsSync(imagesJsonPath)) {
+  const existingData = await Bun.file(imagesJsonPath).text();
+  imagesJson = JSON.parse(existingData);
+}
+
+imagesJson.images.push({
+  link: urls[0] || "",
+  caption: caption,
+});
+
+await Bun.write(imagesJsonPath, JSON.stringify(imagesJson, null, 2));
+console.log("Image uploaded and images.json updated successfully.");
