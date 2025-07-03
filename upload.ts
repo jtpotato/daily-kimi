8985995ba41dffb166e7f6524682fd59cb6c1b2e
Joel <58995538+jtpotato@users.noreply.github.com>
2025-07-03 13:40:15 +1000

    Refactor image upload process and update images.json with new image URL and caption

--- upload.ts
+++ upload.ts
@@ -1,7 +1,6 @@
 import { UTApi } from "uploadthing/server";
 import sharp from "sharp";
 import fs from "fs";
-import { exit } from "process";
 import { execSync } from "child_process";
 
 // read caption from caption.txt
@@ -10,7 +9,15 @@ const caption = await Bun.file("caption.txt").text();
 // read buffer from file named 'imagedump' with no extension
 const imageBuffer = await Bun.file("imagedump").arrayBuffer();
 // convert image to jpeg
-const jpegBuffer = await sharp(imageBuffer).jpeg().toBuffer();
+let jpegBuffer = await sharp(imageBuffer).jpeg().toBuffer();
+// if image width is larger than 1400px width, resize it to 1400px width
+const metadata = await sharp(jpegBuffer).metadata();
+if (metadata.width && metadata.width > 1400) {
+  jpegBuffer = await sharp(jpegBuffer)
+    .resize({ width: 1400 })
+    .jpeg()
+    .toBuffer();
+}
 
 // save to file for testing
 fs.writeFileSync("imagedump.jpg", jpegBuffer);
@@ -41,6 +48,8 @@ imagesJson.images.push({
   caption: caption.trim(),
 });
 
+await Bun.write(imagesJsonPath, JSON.stringify(imagesJson, null, 2));
+
 // Stage the updated images.json file
 execSync("git add images.json");
 
@@ -50,5 +59,4 @@ execSync('git commit -m "Update images.json with new image and caption"');
 // Push the changes to the remote repository
 execSync("git push");
 
-await Bun.write(imagesJsonPath, JSON.stringify(imagesJson, null, 2));
 console.log("Image uploaded and images.json updated successfully.");
